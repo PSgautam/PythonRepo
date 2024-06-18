25bcfa0cb7959063425405269215a1c89c41bb8a
PSgautam <payal131btcseai23@igdtuw.ac.in>
2024-06-18 20:43:25 +0530

    Second commit

--- PythonRepo/file19.py
+++ PythonRepo/file19.py
@@ -1,4 +1,10 @@
 #To remove all punctuation from a string
-
+str1=input("Enter  string:")
+punctuation='''!()-[]{}:;'"\\,<>./?@#$%^&*_'''
+cleaned_string=""
+for char in str1:
+    if char not in punctuation:
+      cleaned_string+=char
+print("String without punctuations:",cleaned_string) 
 
 
